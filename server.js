d03d19d2f89de984a3ccc0d84427903d87a0faa5
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2023-12-12 10:53:31 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const {
+    Pool
+} = require('pg');
+const path = require('path');
+
+const app = express();
+const port = 3080;
+
+// Configurações do banco de dados
+const pool = new Pool({
+    user: 'postgres',
+    host: 'db.mxdyiwlpvxbfoyxbvoxd.supabase.co',
+    database: 'postgres',
+    password: '@Ph974985101',
+    port: 5432,
+});
+
+// Servir arquivos estáticos a partir do diretório 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para obter dados do organograma
+app.post('/api/obterDadosOrganograma', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM "Organograma"');
+        const organogramaData = formatarDadosOrganograma(result.rows);
+        client.release();
+
+        res.json(organogramaData);
+    } catch (error) {
+        console.error('Erro ao obter dados do organograma:', error);
+        res.status(500).json({
+            error: 'Erro ao obter dados do organograma'
+        });
+    }
+});
+
+function formatarDadosOrganograma(rows) {
+    const nodes = [];
+
+    rows.forEach(row => {
+        const node = {
+            id: row.id,
+            name: row.nome,
+            pid: row.idsup,
+            cargo: row.funcao,
+            departamento: row.departamento,
+            // Adicione mais campos conforme necessário
+        };
+
+        nodes.push(node);
+    });
+
+    return nodes;
+}
+
+// Rota para servir o arquivo index.html na raiz ("/")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
